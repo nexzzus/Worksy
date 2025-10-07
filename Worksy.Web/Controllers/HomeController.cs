c138784f70b290881c90a0eae5bed061e0424f2c
Juanka690 <juancardona0606@gmail.com>
2025-10-07 14:30:31 -0500

    Se arreglaron el redireccionamiento entre vistas en el aplicativo y se arreglaron algunos errores basicos

--- Worksy.Web/Controllers/HomeController.cs
+++ Worksy.Web/Controllers/HomeController.cs
@@ -28,4 +28,15 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+    public IActionResult About()
+    {
+        return View();
+    }
+    
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
 }
