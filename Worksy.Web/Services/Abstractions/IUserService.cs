66da0902668cf7566ebe15a3e5c4f6b9ec5cc083
mateo <mateomunoz@gmail.com>
2025-10-21 20:32:48 -0500

    Cambios en UserService

--- Worksy.Web/Services/Abstractions/IUserService.cs
+++ Worksy.Web/Services/Abstractions/IUserService.cs
@@ -1,13 +1,21 @@
+using Microsoft.AspNetCore.Identity;
 using Worksy.Web.Core;
 using Worksy.Web.DTOs;
+using Worksy.Web.ViewModels;
 
 namespace Worksy.Web.Services.Abstractions;
 
 public interface IUserService
 {
-    public Task<Response<UserDTO>> CreateAsync(UserDTO user);
+    public Task<Response<IdentityResult>> AddUserAsync(UserDTO dto, string password);
+    public Task<Response<SignInResult>> LoginAsync(LoginViewModel model);
+    public Task LogoutAsync();
+
+    public Task<Response<IdentityResult>> ConfirmUserAsync(UserDTO dto, string token);
+    public Task<Response<string>> GenerateConfirmationTokenAsync(UserDTO dto);
+
     public Task<Response<object>> DeleteAsync(Guid id);
-    public Task<Response<UserDTO>> GetOneAsync(Guid id);
-    public Task<Response<List<UserDTO>>> GetAllAsync();
-    public Task<Response<UserDTO>> UpdateAsync(UserDTO user);
+    public Task<Response<IdentityResult>> GetOneAsync(Guid id);
+    public Task<Response<List<IdentityResult>>> GetAllAsync();
+    public Task<Response<IdentityResult>> UpdateAsync(UserDTO user);
 }
--- Worksy.Web/Services/Implementations/UserService.cs
+++ Worksy.Web/Services/Implementations/UserService.cs
@@ -1,41 +1,84 @@
-/*using AutoMapper;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Worksy.Web.Core;
 using Worksy.Web.Data;
 using Worksy.Web.Data.Entities;
 using Worksy.Web.DTOs;
 using Worksy.Web.Services.Abstractions;
+using Worksy.Web.ViewModels;
 
 namespace Worksy.Web.Services.Implementations;
 
-public class UserService:CustomQueryableOperationService, IUserService
+public class UserService : IUserService
 {
-    public UserService(DataContext context, IMapper mapper) : base(context, mapper)
+    private readonly DataContext _context;
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    private readonly IMapper _mapper;
+
+    public UserService(DataContext context, UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
     {
-        
+        _context = context;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _mapper = mapper;
     }
 
-    public Task<Response<UserDTO>> CreateAsync(UserDTO user)
+    public async Task<Response<IdentityResult>> AddUserAsync(UserDTO dto, string password)
     {
-        return CreateAsync<User, UserDTO>(user);
+        User user = _mapper.Map<User>(dto);
+        IdentityResult result = await _userManager.CreateAsync(user, password);
+
+        return new Response<IdentityResult>()
+        {
+            Result = result,
+            isSuccess = result.Succeeded
+        };
+    }
+
+    public async Task<Response<SignInResult>> LoginAsync(LoginViewModel model)
+    {
+        SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+        return new Response<SignInResult>
+        {
+            Result = result,
+            isSuccess = result.Succeeded
+        };
+    }
+
+    public async Task LogoutAsync()
+    {
+        await _signInManager.SignOutAsync();
+    }
+
+    public Task<Response<IdentityResult>> ConfirmUserAsync(UserDTO dto, string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Response<string>> GenerateConfirmationTokenAsync(UserDTO dto)
+    {
+        throw new NotImplementedException();
     }
 
     public Task<Response<object>> DeleteAsync(Guid id)
     {
-        return DeleteAsync<User>(id);
+        throw new NotImplementedException();
     }
 
-    public Task<Response<UserDTO>> GetOneAsync(Guid id)
+    public Task<Response<IdentityResult>> GetOneAsync(Guid id)
     {
-        return GetByIdAsync<User, UserDTO>(id);
+        throw new NotImplementedException();
     }
 
-    public Task<Response<List<UserDTO>>> GetAllAsync()
+    public Task<Response<List<IdentityResult>>> GetAllAsync()
     {
-        return GetAllAsync<User, UserDTO>();
+        throw new NotImplementedException();
     }
 
-    public Task<Response<UserDTO>> UpdateAsync(UserDTO user)
+    public Task<Response<IdentityResult>> UpdateAsync(UserDTO user)
     {
-        return UpdateAsync<User, UserDTO>(user, user.Id);
+        throw new NotImplementedException();
     }
-}*/
+}
