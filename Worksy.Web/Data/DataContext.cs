b816015905e83d4e1bf2eb0f17473b2f574c96c8
mateo <mateomunoz@gmail.com>
2025-10-24 16:52:29 -0500

    Configuración de las PK y FK entre los roles y permisos

--- Worksy.Web/Data/DataContext.cs
+++ Worksy.Web/Data/DataContext.cs
@@ -8,9 +8,48 @@ namespace Worksy.Web.Data
     public class DataContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
-        { }
-            public DbSet<Category> Categories { get; set; }
-            public DbSet<Service> Services { get; set;
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Service> Services { get; set; }
+
+        public DbSet<WorksyRole> WorksyRoles { get; set; }
+
+        public DbSet<RolePermission> RolePermissions { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            ConfigureIndex(builder);
+            ConfigureKeys(builder);
+
+            base.OnModelCreating(builder);
+        }
+
+        private void ConfigureKeys(ModelBuilder builder)
+        {
+            // RolePermission
+            builder.Entity<RolePermission>().HasKey(rp => new { rp.PermissionId, rp.WorksyRoleId });
+
+            builder.Entity<RolePermission>()
+                .HasOne(rp => rp.WorksyRole)
+                .WithMany(r => r.RolePermissions)
+                .HasForeignKey(rp => rp.WorksyRoleId);
+
+            builder.Entity<RolePermission>()
+                .HasOne(rp => rp.Permission)
+                .WithMany(r => r.RolePermissions)
+                .HasForeignKey(rp => rp.PermissionId);
+        }
+
+        private void ConfigureIndex(ModelBuilder builder)
+        {
+            // Roles
+            builder.Entity<WorksyRole>().HasIndex(r => r.Name).IsUnique();
+            // Permission
+            builder.Entity<WorksyRole>().HasIndex(r => r.Name).IsUnique();
         }
     }
 }
