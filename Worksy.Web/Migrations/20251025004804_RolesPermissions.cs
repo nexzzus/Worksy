67dff60541f2fa9e470ea5438d388d2746b42b5d
mateo <mateomunoz@gmail.com>
2025-10-24 19:59:37 -0500

    migraciones

---
+++ Worksy.Web/Migrations/20251025004804_RolesPermissions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Worksy.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "WorksyRoleId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Module = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorksyRoles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorksyRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePermissions",
+                columns: table => new
+                {
+                    WorksyRoleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PermissionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePermissions", x => new { x.PermissionId, x.WorksyRoleId });
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_WorksyRoles_WorksyRoleId",
+                        column: x => x.WorksyRoleId,
+                        principalTable: "WorksyRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_WorksyRoleId",
+                table: "AspNetUsers",
+                column: "WorksyRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePermissions_WorksyRoleId",
+                table: "RolePermissions",
+                column: "WorksyRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorksyRoles_Name",
+                table: "WorksyRoles",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_WorksyRoles_WorksyRoleId",
+                table: "AspNetUsers",
+                column: "WorksyRoleId",
+                principalTable: "WorksyRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_WorksyRoles_WorksyRoleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "RolePermissions");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "WorksyRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_WorksyRoleId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WorksyRoleId",
+                table: "AspNetUsers");
+        }
+    }
+}
