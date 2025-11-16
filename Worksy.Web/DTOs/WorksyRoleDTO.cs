a68fd3a1fa9a8a5b67121d5c1406118d8efccc42
mateo <mateomunoz@gmail.com>
2025-11-16 18:02:15 -0500

    ForgotPassword: vista mejorada

--- Worksy.Web/DTOs/WorksyRoleDTO.cs
+++ Worksy.Web/DTOs/WorksyRoleDTO.cs
@@ -5,18 +5,18 @@ namespace Worksy.Web.DTOs;
 public class WorksyRoleDTO
 {
     public Guid Id { get; set; }
-    
+
     [Display(Name = "Role")]
     [Required(ErrorMessage = "El campo {0} es requerido")]
-    [MaxLength(64,ErrorMessage = "El campo {0} debe tener máximo {1} catácteres")]
+    [MaxLength(64, ErrorMessage = "El campo {0} debe tener máximo {1} catácteres")]
     public string Name { get; set; }
-    
-    public string? PermissionsIds {get; set;}
-    
-    public List<PermissionsForRoleDTO> Permissions { get; set; }
+
+    public string? PermissionsIds { get; set; }
+
+    public List<PermissionsForRoleDTO>? Permissions { get; set; }
 }
 
-public class PermissionsForRoleDTO: PermissionDTO
+public class PermissionsForRoleDTO : PermissionDTO
 {
     public bool Selected { get; set; }
 }
